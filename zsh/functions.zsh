2a32e0e068569251d05c7f7b5f856c48daf30416
hophacker <j@paiyou.co>
2019-07-14 20:40:30 +0800

    let stat info present as markdown block

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -153,7 +153,8 @@ function gitcopy() {
   done
   prefix=`git remote get-url origin | sed -E 's/git@github.com:/https:\/\/github.com\//g' | sed -E 's/(.*)\.git/\1/'`
   project_name=`echo $prefix | sed -E 's/.*\/(.*)/\1/'`
-  commits=`git log $commit -n $n --stat --pretty="* [$project_name]($prefix/commit/%H) %an: **%s**"`
+  commits=`git log $commit -n $n --stat --pretty="
+* [$project_name]($prefix/commit/%H) %an: **%s**" | sed 's/^[^*]/> /'`
   which pbcopy &> /dev/null
   if [[ $? == '0' ]]; then
     echo $commits | pbcopy
